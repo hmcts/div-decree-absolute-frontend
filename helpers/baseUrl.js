7203a5e24c34a90b252979da63d989d80093b525
Dominic Taylor <dom.taylor7@gmail.com>
2018-05-22 12:30:52 +0100

    remove unused baseUrl

--- helpers/baseUrl.js
+++
@@ -1,8 +0,0 @@
-const config = require('config');
-
-let baseUrl = `${config.get('node.protocol')}://${config.get('node.hostname')}`;
-if (config.environment === 'development') {
-  baseUrl = `${baseUrl}:${config.get('node.port')}`;
-}
-
-module.exports = baseUrl;
