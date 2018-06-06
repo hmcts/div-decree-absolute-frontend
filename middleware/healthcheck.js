e54fae18c1d1a76a4a04ab8957830d055b2a06c2
Dominic Taylor <dom.taylor7@gmail.com>
2018-06-06 12:51:23 +0100

    lint

--- middleware/healthcheck.js
+++ middleware/healthcheck.js
@@ -2,7 +2,7 @@ const healthcheck = require('@hmcts/nodejs-healthcheck');
 const config = require('config');
 const os = require('os');
 const logger = require('@hmcts/nodejs-logging').Logger.getLogger(__filename);
-const redis = require('services/redis')
+const redis = require('services/redis');
 const outputs = require('@hmcts/nodejs-healthcheck/healthcheck/outputs');
 const { OK } = require('http-status-codes');
 
