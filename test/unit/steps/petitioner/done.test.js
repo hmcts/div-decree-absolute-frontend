8bc9ae81e8071da40b77cc38a141afd890b11a98
John O'Neill <j.oneill@kainos.com>
2019-06-30 22:20:44 +0100

    Created preliminary Done and Exit page for DA (#14)
    * Added DA done page & tests for filters
    
    * Added DA exit page and basic tests

---
+++ test/unit/steps/petitioner/done.test.js
@@ -0,0 +1,54 @@
+const modulePath = 'steps/done/Done.step';
+
+const Done = require(modulePath);
+const idam = require('services/idam');
+const { middleware, sinon, content } = require('@hmcts/one-per-page-test-suite');
+
+describe(modulePath, () => {
+  beforeEach(() => {
+    sinon.stub(idam, 'protect').returns(middleware.nextMock);
+  });
+
+  afterEach(() => {
+    idam.protect.restore();
+  });
+
+  it('has idam.protect and idam.logout middleware', () => {
+    return middleware.hasMiddleware(Done, [idam.protect(), idam.logout()]);
+  });
+
+  it('renders the content', () => {
+    const session = { case: { data: {} } };
+    return content(Done, session, { ignoreContent: ['continue', 'signOut', 'backLink'] });
+  });
+
+  describe('values', () => {
+    it('displays reference number', () => {
+      const session = {
+        case: {
+          data: {
+            caseReference: 'LV17D80101'
+          }
+        }
+      };
+      return content(
+        Done,
+        session,
+        {
+          specificValues: [ session.case.data.caseReference ]
+        }
+      );
+    });
+
+    it('displays petitioner email', () => {
+      const session = { case: { data: { petitionerEmail: 'petitioner@email.com' } } };
+      return content(
+        Done,
+        session,
+        {
+          specificValues: [session.case.data.petitionerEmail]
+        }
+      );
+    });
+  });
+});
