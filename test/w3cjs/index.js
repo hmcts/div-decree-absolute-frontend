dcdf3fdd7b9880cc9005e9a988e59cc8aa9675d6
Marc-steeven Eyeni-Kantsey <mseyeni@icloud.com>
2020-04-03 16:08:21 +0100

    ignore html validation error - role attribute (#102)
    Co-authored-by: mseyeni@icloud.com <marc-steevene@Ilya.lan>

--- test/w3cjs/index.js
+++ test/w3cjs/index.js
@@ -34,7 +34,8 @@ const excludedErrors = [
   "Duplicate ID “dnCosts.claimCosts”.",
   'Attribute “src” not allowed on element “image” at this point.',
   'Element “image” is missing required attribute “height”.',
-  'Element “image” is missing required attribute “width”.'
+  'Element “image” is missing required attribute “width”.',
+  'Bad value “presentation” for attribute “role” on element “svg”.'
 ];
 /* eslint-enable */
 const filteredErrors = r => {
