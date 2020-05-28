16e67fbf777aecd5af6d85999c8f3315d40eb875
Luca <luca.delbuono@hmcts.com>
2020-05-28 11:31:58 +0100

    WLA-103: Translate dates

--- views/filters/date.js
+++ views/filters/date.js
@@ -1,6 +1,8 @@
 const moment = require('moment');
+const i18next = require('i18next');
 
 const date = input => {
+  moment.locale(i18next.language);
   const dateAsMoment = moment(new Date(input));
   if (dateAsMoment.isValid()) {
     return dateAsMoment.format('DD MMMM YYYY');
