36574b63d612f0cac7bd95ef20f1c65f06511e2a
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-11-26 14:58:37 +0000

    log error

--- test/functional/helpers/idamHelper.js
+++ test/functional/helpers/idamHelper.js
@@ -55,6 +55,7 @@ class IdamHelper extends Helper {
           idamConfigHelper.setPin(response.pin);
         })
         .catch(error => {
+          console.log('error creating user is ...', error);// eslint-disable-line no-console
           logger.warnWithReq(
             null, 'idam_user_created',
             'Unable to create IDAM test user/token',
