bf2425f199d7e534573ba1b175b5e97888330560
Tony <anatolysan2@gmail.com>
2021-06-10 18:24:11 +0100

    Fix typo

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ lookAndFeel.configure(app, {
   webpack: {
     entry: [
       path.resolve(__dirname, 'assets/js/main.js'),
-      path.resolve(__dirname, 'assets/js/cookieManager.js'),
+      path.resolve(__dirname, 'assets/js/cookiesManager.js'),
       path.resolve(__dirname, 'assets/scss/main.scss'),
       path.resolve(__dirname, 'assets/scss/_cookies.scss'),
       path.resolve(__dirname, 'assets/scss/_web-chat.scss'),
