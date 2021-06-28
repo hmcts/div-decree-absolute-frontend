1f6f1c66db1dc6edfc0580eb125cbad2b336d19f
James Scott <j.w.scott0@gmail.com>
2021-06-28 13:54:28 +0100

    fixed HTML test issues and remove unused eslint ignore

--- steps/avaya-webchat/AvayaWebchat.step.js
+++ steps/avaya-webchat/AvayaWebchat.step.js
@@ -1,9 +1,8 @@
 const { Page } = require('@hmcts/one-per-page');
-const config = require('config');
 
 class AvayaWebchat extends Page {
   static get path() {
-    return config.paths.avayaWebchat;
+    return '/avaya-webchat';
   }
 }
 
