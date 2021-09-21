26d8cb273049e0912048b2f3836e2247211cc62b
danielbe11 <70265071+danielbe11@users.noreply.github.com>
2021-09-21 12:50:12 +0100

    DFR-65 - Fixing XB tests (#173)
    * Add debugger and change test sequence in petitionerPages
    
    * Uncomment tests
    
    * Remove extendedDebugging

--- test/functional/paths/petitionerPages.js
+++ test/functional/paths/petitionerPages.js
@@ -24,16 +24,34 @@ async function testPetitionerJourney(I, language = 'en') {
   I.testApplyForDecreeAbsolutePage(language);
 
   I.testDonePage(language);
+}
+
+async function testExitPage(I, language = 'en') {
+  await I.retry(2).createAUser(basicDivorceSession.D8PetitionerEmail);
+  I.wait(2);
+  await I.retry(2).createDaCaseForUser(basicDivorceSession);
+
+  I.amOnLoadedPage('/', language);
+
+  await I.testIdamPageForPetitioner();
 
   await I.testExitPage(language);
 }
 
 Feature('Test all pages for Petitioner Journey');
 
-Scenario('Pages with English language preference', async I => {
+Scenario('Complete divorce process (English)', async I => {
   await testPetitionerJourney(I, 'en');
 }).retry(3);
 
-Scenario('Pages with Welsh language preference', async I => {
+Scenario('Exit page (English)', async I => {
+  await testExitPage(I, 'en');
+}).retry(3);
+
+Scenario('Complete divorce process (Welsh)', async I => {
   await testPetitionerJourney(I, 'cy');
+}).retry(3);
+
+Scenario('Exit page (Welsh)', async I => {
+  await testExitPage(I, 'en');
 }).retry(3);
