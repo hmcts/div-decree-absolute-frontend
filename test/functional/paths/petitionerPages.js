5967f0a54016814956fef33e02b652b09f8a71a1
danielbe11 <daniel.bell@solirius.com>
2021-10-19 14:40:31 +0100

    Change tests

--- test/functional/paths/petitionerPages.js
+++ test/functional/paths/petitionerPages.js
@@ -10,22 +10,22 @@ basicDivorceSession.D8PetitionerEmail = emailPrefix + basicDivorceSession.D8Peti
 basicDivorceSession.RespEmailAddress = emailPrefix + basicDivorceSession.RespEmailAddress;
 
 
-// async function testPetitionerJourney(I, language = 'en') {
-//   await I.retry(2).createAUser(basicDivorceSession.D8PetitionerEmail);
-//   I.wait(2);
-//   await I.retry(2).createDaCaseForUser(basicDivorceSession);
-//
-//   I.amOnLoadedPage('/', language);
-//
-//   await I.testIdamPageForPetitioner();
-//
-//   I.testPetProgressBar(language);
-//
-//   I.testApplyForDecreeAbsolutePage(language);
-//
-//   I.testDonePage(language);
-// }
-//
+async function testPetitionerJourney(I, language = 'en') {
+  await I.retry(2).createAUser(basicDivorceSession.D8PetitionerEmail);
+  I.wait(2);
+  await I.retry(2).createDaCaseForUser(basicDivorceSession);
+
+  I.amOnLoadedPage('/', language);
+
+  await I.testIdamPageForPetitioner();
+
+  I.testPetProgressBar(language);
+
+  I.testApplyForDecreeAbsolutePage(language);
+
+  I.testDonePage(language);
+}
+
 // async function testExitPage(I, language = 'en') {
 //   await I.retry(2).createAUser(basicDivorceSession.D8PetitionerEmail);
 //   I.wait(2);
@@ -38,12 +38,12 @@ basicDivorceSession.RespEmailAddress = emailPrefix + basicDivorceSession.RespEma
 //   await I.testExitPage(language);
 // }
 
-// Feature('Test all pages for Petitioner Journey');
-//
-// Scenario('Complete divorce process (English)', async I => {
-//   await testPetitionerJourney(I, 'en');
-// }).retry(3);
-//
+Feature('Test all pages for Petitioner Journey');
+
+Scenario('Complete divorce process (English)', async I => {
+  await testPetitionerJourney(I, 'en');
+}).retry(3);
+
 // Scenario('Exit page (English)', async I => {
 //   await testExitPage(I, 'en');
 // }).retry(3);
