1a14d9412ad9df8ddfc38a3fbae8cbf2bf3f272c
Barry Weeks <barry.weeks@justice.gov.uk>
2022-01-10 17:10:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ lookAndFeel.configure(app, {
       features: {
         antennaWebchat: parseBool(config.features.antennaWebchat),
         antennaWebchatUserAttribute: parseBool(config.features.antennaWebchatUserAttribute),
+        // Dynatrace Feature Toggle
         dynatrace: parseBool(config.features.dynatrace)
       }
     }
