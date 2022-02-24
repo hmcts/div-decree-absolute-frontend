c78bc856cfc54ac4463068a17855048872e8b1fd
Barry Weeks <barry.weeks@justice.gov.uk>
2022-02-24 09:23:56 +0000

    Do not return docs for case
    Do not return docs for respondent in case per DFR-829

--- steps/respondent/progress-bar/RespondentProgressBar.step.js
+++ steps/respondent/progress-bar/RespondentProgressBar.step.js
@@ -14,6 +14,10 @@ class RespondentProgressBar extends ProgressBar {
   }
 
   get downloadableFiles() {
+    if (this.case.caseid === config.hideDocsForCase) {
+      return [];
+    }
+
     const docConfig = {
       documentNamePath: config.document.documentNamePath,
       documentWhiteList: config.document.filesWhiteList.respondent
