84797e13cc89cc2360e6db0f63cf12d1ec83df8f
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2025-06-24 11:06:57 +0100

    Update azurerm provider to v.4.33.0 (#332)
    * Update azurerm provider to v.4.33.0
    
    * Remove redundant terraform module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,146 +3,15 @@ provider "azurerm" {
 }
 
 locals {
-  aseName = "core-compute-${var.env}"
-  public_hostname = "${var.product}-${var.component}-${var.env}.service.${local.aseName}.internal"
-  local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env
-
   previewVaultName = "${var.raw_product}-aat"
   nonPreviewVaultName = "${var.raw_product}-${var.env}"
   vaultName = (var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName
-  evidence_management_client_api_url = "http://div-emca-${local.local_env}.service.core-compute-${local.local_env}.internal"
-
-  case_orchestration_service_api_url = "http://div-cos-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  case_maintence_service_api_url = "http://div-cms-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  fees_payment_service_api_url = "http://div-fps-${local.local_env}.service.core-compute-${local.local_env}.internal"
-
-  health_endpoint = "/health"
-
-  asp_name = var.env == "prod" ? "div-da-prod" : "${var.raw_product}-${var.env}"
-  asp_rg = var.env == "prod" ? "div-da-prod" : "${var.raw_product}-${var.env}"
-
-  appinsights_name           = var.env == "preview" ? "${var.product}-${var.component}-appinsights-${var.env}" : "${var.product}-${var.env}"
-  appinsights_resource_group = var.env == "preview" ? "${var.product}-${var.component}-${var.env}" : "${var.product}-${var.env}"
 }
 
 data "azurerm_subnet" "core_infra_redis_subnet" {
   name                 = "core-infra-subnet-1-${var.env}"
   virtual_network_name = "core-infra-vnet-${var.env}"
   resource_group_name  = "core-infra-${var.env}"
-}
-
-module "frontend" {
-  source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product                         = "${var.product}-${var.component}"
-  location                        = var.location
-  env                             = var.env
-  ilbIp                           = var.ilbIp
-  is_frontend                     = var.env != "preview" ? 1: 0
-  subscription                    = var.subscription
-  additional_host_name            = var.env != "preview" ? var.additional_host_name : "null"
-  https_only                      = "false"
-  capacity                        = var.capacity
-  common_tags                     = var.common_tags
-  asp_name                        = local.asp_name
-  asp_rg                          = local.asp_rg
-  instance_size                   = "I3"
-  appinsights_instrumentation_key = data.azurerm_key_vault_secret.appinsights_secret.value
-  enable_ase                      = var.enable_ase
-
-  app_settings = {
-
-    // Node specific vars
-    NODE_ENV = var.node_env
-    NODE_PATH = var.node_path
-    WEBSITE_NODE_DEFAULT_VERSION = var.node_version
-
-    BASE_URL = "${var.public_protocol}://${local.public_hostname}"
-
-    UV_THREADPOOL_SIZE = var.uv_threadpool_size
-    NODE_CONFIG_DIR = var.node_config_dir
-
-    // Logging vars
-    REFORM_TEAM = var.reform_team
-    component = var.component
-    REFORM_ENVIRONMENT = var.env
-    DEPLOYMENT_ENV=var.deployment_env
-
-    // Packages
-    PACKAGES_NAME=var.packages_name
-    PACKAGES_PROJECT=var.packages_project
-    PACKAGES_ENVIRONMENT=var.packages_environment
-    PACKAGES_VERSION=var.packages_version
-
-    // Service name
-    SERVICE_NAME=var.frontend_service_name
-
-    // IDAM
-    IDAM_API_URL = var.idam_api_url
-    IDAM_APP_HEALHCHECK_URL ="${var.idam_api_url}${var.health_endpoint}"
-    IDAM_LOGIN_URL = "${var.idam_authentication_web_url}${var.idam_authentication_login_endpoint}"
-    IDAM_AUTHENTICATION_HEALHCHECK_URL = "${var.idam_authentication_web_url}${var.health_endpoint}"
-    IDAM_SECRET = data.azurerm_key_vault_secret.idam_secret.value
-
-    // Redis Cloud
-    REDIS_ENCRYPTION_SECRET = data.azurerm_key_vault_secret.redis_secret.value
-
-    // Evidence Management Client API
-    EVIDENCE_MANAGEMENT_CLIENT_API_URL             = local.evidence_management_client_api_url
-    EVIDENCE_MANAGEMENT_CLIENT_API_HEALTHCHECK_URL = "${local.evidence_management_client_api_url}${local.health_endpoint}"
-    EVIDENCE_MANAGEMENT_CLIENT_API_UPLOAD_ENDPOINT = var.evidence_management_client_api_upload_endpoint
-    EVIDENCE_MANAGEMENT_CLIENT_API_DOWNLOAD_ENDPOINT = var.evidence_management_download_endpoint
-
-    // Case Orchestration API
-    ORCHESTRATION_SERVICE_URL              = local.case_orchestration_service_api_url
-    ORCHESTRATION_SERVICE_GET_PETITION_URL = "${local.case_orchestration_service_api_url}/retrieve-case"
-    ORCHESTRATION_SERVICE_GET_AOS_PETITION_URL = "${local.case_orchestration_service_api_url}/retrieve-aos-case"
-    ORCHESTRATION_SERVICE_POST_PETITION_URL= "${local.case_orchestration_service_api_url}/submit-da"
-    ORCHESTRATION_SERVICE_HEALTH_URL       = "${local.case_orchestration_service_api_url}${local.health_endpoint}"
-    ORCHESTRATION_SERVICE_DRAFT_ENDPOINT   = var.case_orchestration_service_draft_endpoint
-
-    //Case Maintenance
-    CASE_MAINTENANCE_BASE_URL              = local.case_maintence_service_api_url
-
-    //Fees and Payments
-    FEES_AND_PAYMENTS_URL = local.fees_payment_service_api_url
-    FEES_AND_PAYMENTS_HEALTHCHECK_URL = "${local.fees_payment_service_api_url}${local.health_endpoint}"
-
-    // Feature toggling through config
-    FEATURE_IDAM                            = var.feature_idam
-    FEATURE_WEBCHAT                         = var.feature_webchat
-
-    // Encryption secrets
-    SESSION_SECRET = data.azurerm_key_vault_secret.session_secret.value
-
-    // Google Anayltics
-    GOOGLE_ANALYTICS_ID           = var.google_analytics_tracking_id
-    GOOGLE_ANALYTICS_TRACKING_URL = var.google_analytics_tracking_url
-
-    // HPKP
-    HPKP_MAX_AGE = var.hpkp_max_age
-    HPKP_SHAS = var.hpkp_shas
-
-    // Rate Limiter
-    RATE_LIMITER_TOTAL  = var.rate_limiter_total
-    RATE_LIMITER_EXPIRE = var.rate_limiter_expire
-    RATE_LIMITER_ENABLED = var.rate_limiter_enabled
-
-    // Petitioner Front End
-    PETITIONER_FRONTEND_URL = var.petitioner_frontend_url
-
-    // Respondent Front End
-    RESPONDENT_FRONTEND_URL = var.respondent_frontend_url
-
-    // Decree Nisi Front End
-    DECREE_NISI_FRONTEND_URL = var.decree_nisi_frontend_url
-
-    // CCD Filters
-    CCD_DIGITAL_COURTS = var.ccd_digital_courts
-
-    WEBSITE_LOCAL_CACHE_OPTION = "Never"
-    WEBSITE_LOCAL_CACHE_SIZEINMB = 0
-    WEBSITE_DYNAMIC_CACHE = 0
-  }
 }
 
 data "azurerm_key_vault" "div_key_vault" {
